ae5da33148d6d87bb5b5b3ae96a8cd5342ff31c0
UnimaginableCat <CyberEgor4>
2022-06-05 14:33:14 +0300

    Working exporting from retail to zonesmart

--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,7 @@
 import json
 import string
 
+import jsonpickle as jsonpickle
 import requests
 import retailcrm
 from django.http import HttpResponsePermanentRedirect
@@ -20,8 +21,11 @@ def get_access_token(refresh_token):
     }
     response = requests.post("https://api.zonesmart.com/v1/auth/jwt/refresh/", headers=header, json=data)
     converted_response = json.loads(response.text)
-    access_token = converted_response['access']
-    return access_token
+    if response.status_code == 200:
+        access_token = converted_response['access']
+        return True, access_token
+    else:
+        return False, ""
 
 
 def check_zone_cookies(request):
@@ -38,7 +42,7 @@ def check_zone_cookies(request):
     if email is None or password is None or refresh_token is None or access_token is None:
         return False, "", "", "", ""
     else:
-        return True, email, password, refresh_token
+        return True, email, password, refresh_token, access_token
 
 
 def check_retail_cookies(request):
@@ -65,27 +69,39 @@ def try_zone_login(access_token):
     else:
         return False
 
+
 class ZoneSmartProduct:
     def __init__(self, sku, quantity, price, product_code, main_image, extra_images):
         self.sku = sku
         self.quantity = quantity
         self.price = price
-        self.product_code = product_code
+        if product_code is None:
+            self.product_code = "pcode"
+        else:
+            self.product_code = product_code
         self.condition = "NEW"
         self.main_image = main_image
         self.extra_images = extra_images
+        #self.attributes = {"name": "testName",
+        #                   "value":"testValue"}
 
 
 class ZoneSmartListing:
     def __init__(self, title, desc, list_sku, cat, cat_name, brand, products: list):
         self.title = title
-        self.description = desc
+        if desc == "":
+            self.description = "Exported from RetailCRM"
+        else:
+            self.description = desc
         self.listing_sku = list_sku
-        self.category = cat
+        #self.category = cat
         self.category_name = cat_name
         self.brand = brand
         self.currency = "RUB"
         self.products = products
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,ensure_ascii=False)
 
 
 class RetailCRMHelper:
@@ -168,6 +184,21 @@ def init_retail_helper(request):
     retail_helper.init_and_setup(address, api_key)
     return retail_helper
 
+#Ура почти заработало
+def zone_create_listing(products, access_token):
+    header = {
+        'Content-Type': 'application/json',
+        'Authorization': 'JWT ' + access_token
+    }
+    results = dict()
+    for product in products:
+        json_product = product.to_json()
+        correct_json = json.loads(json_product)
+        response = requests.post("https://api.zonesmart.com/v1/zonesmart/listing/", headers=header,
+                                 json=correct_json)
+        results[product.title] = response.status_code
+    return results
+
 
 class ExportProductsView(View):
     template_name = 'main_page/main_export_products.html'
@@ -181,30 +212,44 @@ class ExportProductsView(View):
         active = request.POST.get('active')
         min_quantity = request.POST.get('min_quantity')
         groups = request.POST.getlist('groups')
-        # Список продуктов для переноса
         products_to_export = retail_helper.get_products(groups_dict, active, min_quantity, groups)
+        print(products_to_export)
+
+        zone_cookie_check, email, password, refresh_token, access_token = check_zone_cookies(request)
+        match zone_cookie_check:
+            case True:
+                access_token_check = try_zone_login(access_token)
+                if access_token_check:
+                    zone_create_listing(products_to_export, access_token)
+                    return render(request, self.template_name,
+                                  context={"form": form, "zone_status": try_zone_login(access_token)})
+                else:
+                    refresh_check, new_access_token = get_access_token(refresh_token)
+                    if refresh_check:
+                        request.session['access_token'] = new_access_token
+                        zone_create_listing(products_to_export, new_access_token)
+                        return render(request, self.template_name,
+                                      context={"form": form, "zone_status": try_zone_login(new_access_token)})
+                    else:
+                        return render(request, self.template_name, context={"form": form,
+                                                                            "zone_status": False,
+                                                                            "export_error": True})
+            case False:
+                return render(request, self.template_name,
+                              context={'zone_status': False,
+                                       'form': form})
 
 
-        #print(products_to_export)
-        zone_cookie_check, email, password = check_zone_cookies(request)
-        if zone_cookie_check:
-            return render(request, self.template_name,
-                          context={'zone_status': auth_helper.create_zone_tokens(email, password),
-                                   'form': form})
-        else:
-            return render(request, self.template_name,
-                          context={'zone_status': False,
-                                   'form': form})
 
     def get(self, request):
         retail_helper = init_retail_helper(request)
         choices, groups_dict = retail_helper.get_product_groups()
         #print(choices)
         form = ExportProductsForm(choices)
-        zone_cookie_check, email, password = check_zone_cookies(request)
+        zone_cookie_check, email, password, refresh_token, access_token = check_zone_cookies(request)
         if zone_cookie_check:
             return render(request, self.template_name,
-                          context={'zone_status': auth_helper.create_zone_tokens(email, password),
+                          context={'zone_status': try_zone_login(access_token),
                                    'form': form})
         else:
             return render(request, self.template_name,
@@ -226,7 +271,7 @@ class ZoneAccountView(View):
             request.session['zone_email'] = email
             request.session['zone_pass'] = password
             request.session['access_token'] = access_token
-            request.session['refresh_token'] = access_token
+            request.session['refresh_token'] = refresh_token
             return render(request, "main_page/main_zone_acc.html", context={"form": form,
                                                                             "auth_state": zone_login_check,
                                                                             "zone_status": zone_login_check})
@@ -235,22 +280,28 @@ class ZoneAccountView(View):
                                                                             "auth_state": zone_login_check})
 
     def get(self, request):
-        zone_cookie_check, email, password, refresh_token = check_zone_cookies(request)
-        if zone_cookie_check:
-            access_token = get_access_token(refresh_token)
-            zone_login_check = try_zone_login(access_token)
-            if zone_login_check:
-                auth_helper.set_zone_pass(password)
-                auth_helper.set_zone_email(email)
-                form = ZoneSmartLoginForm(initial={'email': email,
-                                                   'password': password}, auto_id=False)
-                return render(request, "main_page/main_zone_acc.html", context={"form": form,
-                                                                                "zone_status": zone_login_check})
-            else:
+        zone_cookie_check, email, password, refresh_token, access_token = check_zone_cookies(request)
+        form = ZoneSmartLoginForm(initial={'email': email,
+                                           'password': password}, auto_id=False)
+        match zone_cookie_check:
+            case True:
+                access_token_check = try_zone_login(access_token)
+                if access_token_check:
+                    return render(request, "main_page/main_zone_acc.html", context={"form": form,
+                                                                                    "zone_status": try_zone_login(
+                                                                                        access_token)})
+                else:
+                    refresh_check, new_access_token = get_access_token(refresh_token)
+                    if refresh_check:
+                        request.session['access_token'] = new_access_token
+                        return render(request, "main_page/main_zone_acc.html", context={"form": form,
+                                                                                        "zone_status": try_zone_login(
+                                                                                            new_access_token)})
+                    else:
+                        form = ZoneSmartLoginForm(request.GET)
+                        return render(request, "main_page/main_zone_acc.html", context={"form": form,
+                                                                                        "zone_status": False})
+            case False:
                 form = ZoneSmartLoginForm(request.GET)
                 return render(request, "main_page/main_zone_acc.html", context={"form": form,
-                                                                                "zone_status": zone_login_check})
-        else:
-            form = ZoneSmartLoginForm(request.GET)
-            return render(request, "main_page/main_zone_acc.html", context={"form": form,
-                                                                            "zone_status": False})
+                                                                                "zone_status": False})
